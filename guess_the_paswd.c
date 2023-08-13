fdce0d96d9f08095185ab57470d16dd8ad34bdcb
Rajneeshkarya <125280130+Rajneeshkarya@users.noreply.github.com>
2023-08-13 18:47:07 +0530

    Add files via upload

---
+++ guess_the_paswd.c
@@ -0,0 +1,64 @@
+//Reverse_Engineering game
+//Created by :- Rajneesh K. Arya
+
+#include <stdio.h>
+#include <string.h>
+
+int eval(){
+    int a = (((((((10 / 10) + 10)/10)-10)*10)-10)/10)+10;
+    further(a);
+}
+int further(int a){
+    a = (((a+sizeof(int)+sizeof(char))*10)-20)/10;
+    return a;
+}
+
+int main(){
+
+    char hash[20] = "r90o84g43g48u31lyh";
+    char salt[12] = "9084434831";
+    char new_pass[19];
+    char int_pass[9];
+
+    int l = strlen(hash);
+    int sl = strlen(salt);
+
+
+    printf("Your Task is to guess the password (HINT : Analyse the code to retrieve the password)\n\n");
+
+    printf("Password:");
+    fgets(int_pass,9,stdin);
+
+    int i=0,j=0,k=0;
+
+
+    while(i<l){
+        if(i%3==0 || i == 0){
+            new_pass[i] = int_pass[j] + eval();
+            j++;
+        }
+        else{
+            if(k<10){
+                new_pass[i] = salt[k];
+                k++;
+            }
+            else{
+                new_pass[i] = int_pass[j] + eval();
+                j++;
+            }
+        }
+        i++;
+    }
+
+    new_pass[l] = '\0';
+
+    int ans = strcmp(hash,new_pass);
+    
+    if(!ans){
+        printf("YOu got a correct Answer\n");
+    }
+    else{
+        printf("Wrong Answer !!!!\nYou need more Practice\n");
+    }
+    return 0;
+}
